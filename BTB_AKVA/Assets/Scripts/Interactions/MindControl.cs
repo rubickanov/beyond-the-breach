246722487a278fe6c385854b53cfb70f21326730
Alexander Miroshnichenko <a.miroshnichenko@student.saedubai.com>
2023-11-01 10:30:03 +0400

    Added mind controlling

--- BTB_AKVA/Assets/Scripts/Interactions/MindControl.cs
+++ BTB_AKVA/Assets/Scripts/Interactions/MindControl.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using AKVA.Player;
+using System.Collections;
+using System;
 
 namespace AKVA.Interaction
 {
@@ -10,33 +12,68 @@ namespace AKVA.Interaction
         [SerializeField] private float distanceToMindControl;
 
         [SerializeField] private GameObject controlUI;
+        [SerializeField] private GameObject cancelControlUI;
 
         private MindControlledObject mindControlledObject;
+
+        private bool isControlling = false;
+
+
         private void Update()
         {
             RaycastHit hit;
-            if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, distanceToMindControl))
+            if(!isControlling)
             {
-                if (hit.transform.TryGetComponent(out mindControlledObject))
+                cancelControlUI.SetActive(false);
+                if(Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, distanceToMindControl))
                 {
-                    controlUI.SetActive(true);
-                    if (Input.GetKeyDown(PlayerInput.Instance.Controls.interact))
+                    if(hit.transform.TryGetComponent(out mindControlledObject))
                     {
-                        Control(mindControlledObject.transform);
+                        controlUI.SetActive(true);
+                        if(Input.GetKeyDown(PlayerInput.Instance.Controls.interact))
+                        {
+                            Control(mindControlledObject.transform);
+                        }
                     }
                 }
-            }
-            else
+                else
+                {
+                    controlUI.SetActive(false);
+                }
+            } else
             {
                 controlUI.SetActive(false);
+                cancelControlUI.SetActive(true);
+                if(Input.GetKeyDown(PlayerInput.Instance.Controls.interact))
+                {
+                    ReturnToBody(mindControlledObject.transform);
+                }
             }
         }
         
         public void Control(Transform objectTransform)
         {
-            var position = transform.position;
-            transform.position = objectTransform.position;
-            objectTransform.position = position;
+            Swap(objectTransform);
+            isControlling = true;
+        }
+
+        private void ReturnToBody(Transform objectTransform)
+        {
+            Swap(objectTransform);
+            isControlling = false;
+        }
+
+        private void Swap(Transform objectTransform)
+        {
+            var position = objectTransform.position;
+            Vector3 forward = objectTransform.forward;
+            CharacterController controller = GetComponent<CharacterController>();
+            controller.enabled = false;
+            objectTransform.position = transform.position;
+            objectTransform.forward = transform.forward;
+            transform.position = position;
+            transform.forward = forward;
+            controller.enabled = true;
         }
         
         private void OnDrawGizmosSelected()
