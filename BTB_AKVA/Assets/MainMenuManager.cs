623a32dab98e26505cb389ade6ddbfd0c1b68388
Alexander Miroshnichenko <a.miroshnichenko@student.saedubai.com>
2023-12-14 17:24:27 +0400

    Added continue button

--- BTB_AKVA/Assets/MainMenuManager.cs
+++ BTB_AKVA/Assets/MainMenuManager.cs
@@ -11,18 +11,28 @@ namespace AKVA
     {
         [SerializeField] private AudioSource audioSource;
         [SerializeField] private Button quitButton;
-
+        [SerializeField] private Button continueButton; 
+        
         [SerializeField] private FloatReference bgMusicVolume;
 
         private void Start()
         {
             quitButton.onClick.AddListener(QuitGame);
+
+
+            if (!PlayerPrefs.HasKey("MainSceneUnlocked"))
+            {
+                Debug.Log("PLAYER PREF HAS NOT BEEN FOUND");
+                continueButton.interactable = false;
+            }
         }
 
         private void Update()
         {
             audioSource.volume = bgMusicVolume.value / 100f;
         }
+        
+        
 
         private void QuitGame()
         {
